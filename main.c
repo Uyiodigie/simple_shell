8e99fc49606ffa44a6bb8cd5126c57908aaa016e
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-08-08 14:06:55 -0700

    update files

--- main.c
+++ main.c
@@ -9,10 +9,11 @@ void free_dat(data_shell *datastruct)
 {
 	unsigned int i;
 
-	for (i = l0; datasruct->_environ[i]; i++)
+	for (i = 1; datastruct->_environ[i]; i++)
 		free(datastruct->_environ[i]);
 	free(datastruct->_environ);
 	free(datastruct->pid);
+}
 
 /**
  * set_dat - initialize data structure
@@ -33,11 +34,30 @@ void set_dat(data_shell *datastruct, char **argv)
 	for (i = 0; environ[i]; i++)
 		;
 	datastruct->_environ = malloc(sizeof(char *) * (i + 1));
-	for (i
+	for (i = 0; environ[i]; i++)
+		datastruct->_environ[i] = _strdup(environ[i]);
+	datastruct->_environ[i] = NULL;
+	datastruct->pid = aux_itoa(getpid());
+}
+
 /**
- * main - the main function for the simple shell
+ * main - the main function for the simple shelli
+ * @argc: argument count
+ * @argv: argument vector
  * Description: The program is a custom made UNIX shell.
  * It was created to immitate most of its basic features and functions.
- * Return:
+ * Return: 0 if success
  */
-int main()
+int main(int argc, char **argv)
+{
+	data_shell datastruct;
+	(void)argc;
+
+	signal(SIGINT, get_sigint);
+	set_dat(&datastruct, argv);
+	shell_loop(&datastruct);
+	free_dat(&datastruct);
+	if (datastruct.status < 0)
+		return (255);
+	return (datastruct.status);
+}
