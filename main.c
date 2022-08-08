811da679308133cc299f0792b6e58f4b071e4a6d
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-08-08 13:29:13 -0700

    update main.c

--- main.c
+++ main.c
@@ -1,4 +1,39 @@
-#include "main.h"
+#include "header.h"
+
+/**
+ * free_dat - frees the data structure
+ * @datastruct: data structure
+ * Return: void
+ */
+void free_dat(data_shell *datastruct)
+{
+	unsigned int i;
+
+	for (i = l0; datasruct->_environ[i]; i++)
+		free(datastruct->_environ[i]);
+	free(datastruct->_environ);
+	free(datastruct->pid);
+
+/**
+ * set_dat - initialize data structure
+ * @datastruct: data structure
+ * @argv: argument vector
+ * Return: void
+ */
+void set_dat(data_shell *datastruct, char **argv)
+	{
+		unsigned int i;
+
+		datastruct->argv = argv;
+		datastruct->input = NULL;
+		datastruct->args = NULL;
+		datastruct->status = 0;
+		datastruct->counter = 1;
+
+		for (i = 0; environ[i]; i++)
+			;
+		datastruct->_environ = malloc(sizeof(char *) * (i + 1));
+		for (i = 0; environ[i] = _strdup(environ[i]);
 
 /**
  * main - the main function for the simple shell
